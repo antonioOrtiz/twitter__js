ca58eeb7609dd7343bf478df4915fcf311a25a1d
nvanderp <nvanderp@gmail.com>
2017-11-06 18:30:42 -0500

    getting there

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 
+
 const PORT = 3000;
+
+app.use('/', (req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
 
 app.get('/', function(req, res) {
   res.send('Welcome to the root route');
